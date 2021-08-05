ecfe9db7b97b2419112406800379958f63719020
Onur Yugen <onuryugen@hotmail.com>
2021-08-05 17:04:00 +0300

    Ubuntu

--- new.js
+++ new.js
@@ -1,4 +1,3 @@
-console.log("Hello World");
 function computerPlay () {
     let luckyNum = Math.random();
     if (luckyNum < 0.333) {
@@ -22,31 +21,71 @@ function playRound (playerSelection, computerSelection) {
          return "Player won";
      };
   }
-function game () {
-    let comp = 0;
-    let pla = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, paper or scissors?");
+  let comp = 0;
+  let pla = 0;
+  document.getElementById("playerscore").innerHTML = pla;
+  document.getElementById("computerscore").innerHTML = comp;
+  function game () {
         const computerSelection = computerPlay();
         let round = playRound(playerSelection, computerSelection);
             if (round == "Computer won") {
                 comp++;
+                document.getElementById("computerscore").innerHTML = comp;
             } else if (round == "Player won") {
                 pla++;
-            };
+                document.getElementById("playerscore").innerHTML = pla;
+            };    
         console.log(round);
-    };
-    if (comp > pla) {
-        return "You've lost";
-    } else if (pla > comp) {
-        return "You've won"
-    } else {
-        return "Draw";
-    }
-}
-console.log(game());
+        if (comp === 5) {
+            comp = 0;
+            pla = 0;
+            document.getElementById("playerscore").innerHTML = pla;
+            document.getElementById("computerscore").innerHTML = comp;
+            alert("Game over, You've defeated");
+            return "Game over, You've defeated";
+        } else if (pla === 5) {
+            comp = 0;
+            pla = 0;
+            document.getElementById("playerscore").innerHTML = pla;
+            document.getElementById("computerscore").innerHTML = comp;
+            alert("Game over, You've won");
+            return "Game over, You've won";
+        } else {
+            return "The battle continues";
+        }
+  }
 
-
+// This is the start for building UI
+    document.getElementById("rock").addEventListener("click", function () {
+        playerSelection = "rock";
+        console.log(game());
+    });
+    document.getElementById("paper").addEventListener("click", function () {
+        playerSelection = "paper";
+        console.log(game());
+    });
+    document.getElementById("scissors").addEventListener("click", function () {
+        playerSelection = "scissors";
+        console.log(game());
+    });
+    document.getElementById("rock").addEventListener("mouseover", function () { 
+        document.getElementById("rock").classList.add("mouseover");    
+    });
+    document.getElementById("rock").addEventListener("mouseleave", function () {
+        document.getElementById("rock").classList.remove("mouseover");    
+    });
+    document.getElementById("paper").addEventListener("mouseover", function () {
+        document.getElementById("paper").classList.add("mouseover");    
+    });
+    document.getElementById("paper").addEventListener("mouseleave", function () {
+        document.getElementById("paper").classList.remove("mouseover");    
+    });
+    document.getElementById("scissors").addEventListener("mouseover", function () {
+        document.getElementById("scissors").classList.add("mouseover");    
+    });
+    document.getElementById("scissors").addEventListener("mouseleave", function () {
+        document.getElementById("scissors").classList.remove("mouseover");    
+    });
  
  
   
